cee64e7d4d891042a1e7599cae8abd9ad00d7c3e
hxwang <wang1570@e.ntu.edu.sg>
2024-06-27 08:02:22 +0000

    [impl] add numba matmul

--- kernels/matmul/main.py
+++ kernels/matmul/main.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import importlib
 
 import torch
 from torch.testing import assert_allclose
@@ -39,7 +41,20 @@ def test_matmul_tiled():
 
     print(profile(ext.matmul_tiled, m1, m2))
 
+def test_matmul_tiled_numba():
+    spec = importlib.util.spec_from_file_location('ext', f"{CUR_DIR}/matmul_tiled_numba.py")
+    ext = importlib.util.module_from_spec(spec)
+    sys.modules['ext'] = ext
+    spec.loader.exec_module(ext)
+
+    m1 = torch.randn(MAT_SIZE, MAT_SIZE, device='cuda')
+    m2 = torch.randn(MAT_SIZE, MAT_SIZE, device='cuda')
+
+    assert_allclose(ext.matmul_2d_numba(m1, m2, 32), m1 @ m2)
+
+    print(profile(ext.matmul_2d_numba, m1, m2, 32))
 
 if __name__ == '__main__':
     test_matmul_simple()
     test_matmul_tiled()
+    test_matmul_tiled_numba()
